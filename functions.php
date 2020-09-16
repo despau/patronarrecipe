7f1722f4e4544f87486e042bdfde8e690be1df97
raug-dev24 <romanegustin24@gmail.com>
2020-09-16 19:26:16 -0400

    added menu support

--- functions.php
+++ functions.php
@@ -8,11 +8,13 @@ define('patronarrecipe_DEV_MODE', true);
 
 // Includes
 include( get_theme_file_path( '/includes/front/enqueue.php' ));
+include( get_theme_file_path( '/includes/setup.php' ));
 
 
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'patronarrecipe_enqueue' );
+add_action( 'after_setup_theme', 'patronarrecipe_setup_theme' );
 
 
 
---
+++ includes/setup.php
@@ -0,0 +1,8 @@
+<?php
+
+
+function patronarrecipe_setup_theme() {
+
+    register_nav_menu( 'primary', __( 'Primary Menu', 'patronarrecipe' ) );
+
+}
