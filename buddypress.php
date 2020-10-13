cec04c1d1204ddb5b37f132f7feeb0d863c16ab9
raug-dev24 <romanegustin24@gmail.com>
2020-10-12 23:04:24 -0400

    getting started with buddypress, and created a budypress.php file

---
+++ buddypress.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<!-- Page Title
+============================================= -->
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+    <div class="content-wrap">
+
+        <div class="container clearfix">
+            <?php
+
+            while( have_posts() ){
+                the_post();
+
+                the_content();
+
+            }
+
+            ?>
+        </div>
+
+    </div>
+
+    <h5>template: full-width-page.php</h5>
+</section><!-- #content end -->
+
+<?php get_footer(); ?>
